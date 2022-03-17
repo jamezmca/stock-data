3d78f151bda6110290e050e930471fa5b221e915
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-03-17 15:33:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/:stock', async (req, res) => {
             const { data } = await axios.get(url)
 
             const $ = cheerio.load(data)
-            
+
             if (type === 'history') {
                 const prices = $('td:nth-child(6)').get().map(val => $(val).text())
                 return { prices }
@@ -56,37 +56,13 @@ app.get('/:stock', async (req, res) => {
                     'Total Debt/Equity (mrq)',
                     'Operating Cash Flow (ttm)'
                 ]
-                // console.log($('section[data-test="qsp-statistics"]').get())
-                const statsArea = $('section[data-test="qsp-statistics"] > div:nth-child(2)').get().map(val => {
-                    const $ = cheerio.load(val)
-                    res.send($.html())
-                    // console.log($('div:first-child tbody tr').get())
-                    const valuationMeasures = $('div:first-child tbody tr').get().map(val => {
-                        const text = $(val).text()
-                        console.log(text)
-                        if (metrics.reduce((acc, curr) => {
-                            if (acc === true) { return true }
-                            return $(val).text().includes(curr)
-                        }, false)) {
-                            return text
-                        } else {
-                            return ''
-                        }
-                    }).filter(val => val !== '').reduce((acc, curr) => {
-                        let title = ''
-                        for (const titl of metrics) {
-                            if (curr.includes(titl)) {
-                                title = titl
-                            }
-                        }
-                        return {...acc, [title]: curr.replace(title, '')}
-                    }, {})
-                    return  valuationMeasures
-                })
-                return {financials: statsArea[0]}
+                console.log($('section[data-test="qsp-statistics"]').get())
+              
+
+                return { financials: statsArea[0] }
             }
         }))
-
+        
 
         // res.status(200).send({
         //     [stock]: stockInfoType.reduce((acc, curr) => {
