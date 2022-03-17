ed8c3a0a55f18af9cea7936f444e2af17a1bd3ee
James McArthur <77163783+jamezmca@users.noreply.github.com>
2022-03-17 14:56:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/:stock', async (req, res) => {
                 // console.log($('section[data-test="qsp-statistics"]').get())
                 const statsArea = $('section[data-test="qsp-statistics"] div:nth-child(2)').get().map(val => {
                     const $ = cheerio.load(val)
-                    console.log($('div:first-child tbody tr').get())
+                    // console.log($('div:first-child tbody tr').get())
                     const valuationMeasures = $('div:first-child tbody tr').get().map(val => {
                         const text = $(val).text()
                         console.log(text)
